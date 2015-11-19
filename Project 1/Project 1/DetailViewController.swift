6b3a9a9999fd2fc88edd085d23128a6fac48ea5a
Aishwarya-Mac2 <ashv266@gmail.com>
2015-11-19 08:17:46 -0500

    Initial commit on new branch

--- Project 1/Project 1/DetailViewController.swift
+++ Project 1/Project 1/DetailViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailmageView: UIImageView!
-
+//Last addition made
 
     var detailItem: AnyObject? {
         didSet {
